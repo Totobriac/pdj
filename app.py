3e3ae1b2c9ba7d5d56e55e0f4790bf168c706e3c
Vincent Cailly <53277392+Totobriac@users.noreply.github.com>
2024-03-17 20:43:54 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,14 +7,14 @@ app = Flask(__name__)
 def calcul_patisseries_et_baguettes(nb_petits_dejeuners):
     # Calcul du nombre total de croissants et de pains au chocolat nécessaires
     if nb_petits_dejeuners > 10:
-        nb_croissants_par_personne = math.ceil(0.75)
-        nb_pains_chocolat_par_personne = math.ceil(0.75)
+        nb_croissants_par_personne = 0.75
+        nb_pains_chocolat_par_personne = 0.75
     else:
         nb_croissants_par_personne = 1
         nb_pains_chocolat_par_personne = 1
     
-    nb_croissants = nb_petits_dejeuners * nb_croissants_par_personne
-    nb_pains_chocolat = nb_petits_dejeuners * nb_pains_chocolat_par_personne
+    nb_croissants = math.ceil(nb_petits_dejeuners * nb_croissants_par_personne)
+    nb_pains_chocolat = math.ceil(nb_petits_dejeuners * nb_pains_chocolat_par_personne)
 
     # Calcul du nombre de baguettes nécessaires
     nb_baguettes = math.ceil(nb_petits_dejeuners / 3)  # Une baguette pour 3 personnes, arrondie à l'entier supérieur
